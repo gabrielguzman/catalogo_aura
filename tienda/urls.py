c1b6b1b81a4f6427a6e89567f5a1e54480100428
Gabriel Guzman <gabrielguzman147.gg@gmail.com>
2026-01-24 13:25:01 -0300

    mejoras mas paginas de nosotros y contacto

--- tienda/urls.py
+++ tienda/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.catalogo, name='catalogo'),
     path('categoria/<slug:category_slug>/', views.catalogo, name='productos_por_categoria'),
+    path('contacto/', views.contacto, name='contacto'),
+    path('nosotros/', views.nosotros, name='nosotros'),
 ]
--- tienda/views.py
+++ tienda/views.py
@@ -1,22 +1,30 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Producto, Categoria
+from django.db.models import Q
 
 def catalogo(request, category_slug=None):
     category = None
     categorias = Categoria.objects.all()
-    
-    # 1. Lógica de selección de categoría
+    productos = Producto.objects.filter(activo=True)
+
     if category_slug:
         category = get_object_or_404(Categoria, slug=category_slug)
-    else:
-        category = Categoria.objects.filter(slug='lenceria').first()
-        if not category:
-            category = categorias.first()
-
-    productos = Producto.objects.filter(categoria=category, activo=True)
+        productos = productos.filter(categoria=category)
+    
+    query = request.GET.get('q') # 'q' es lo que escribe el usuario en la cajita
+    if query:
+        productos = productos.filter(
+            Q(nombre__icontains=query) | Q(descripcion__icontains=query)
+        )
 
     return render(request, 'tienda/catalogo.html', {
         'category': category,
         'categorias': categorias,
         'productos': productos
     })
+
+def contacto(request):
+    return render(request, 'tienda/contacto.html')
+
+def nosotros(request):
+    return render(request, 'tienda/nosotros.html')
